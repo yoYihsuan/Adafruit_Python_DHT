b5268dbb449cc1ac140c063d33e2df13895ba81b
yoYihsuan <weachi0105@gmail.com>
2018-11-27 21:08:52 +0800

    AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -24,6 +24,9 @@ import Adafruit_DHT
 import time
 import httplib, urllib
 import json
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 deviceId = "DfsLIp2l"
 deviceKey = "cu5w0dxIsK1GTHWE"
 def post_to_mcs(payload):
@@ -71,12 +74,13 @@ else:
 # If this happens try again!
 
 while True:
+	SwitchStatus = GPIO.input(14)
 	h0, t0= Adafruit_DHT.read_retry(11, 4)
 	if h0 is not None and t0 is not None:
 		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
 
 		payload = {"datapoints":[{"dataChnId":"humidity","values":{"value":h0}},
-			{"dataChnId":"temperature","values":{"value":t0}}]}
+			{"dataChnId":"temperature","values":{"value":t0}},{"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]}
 		post_to_mcs(payload)
 		time.sleep(10)
 
